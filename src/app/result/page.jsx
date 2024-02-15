85130ce8938e007da0d7bc45492d912a3ce22f08
秉虎 <s96016641@gmail.com>
2024-02-16 01:09:32 +0800

    ✨ Update layout

--- src/app/result/page.jsx
+++ src/app/result/page.jsx
@@ -4,6 +4,8 @@ import { useSearchParams } from "next/navigation";
 import useSWRImmutable from "swr/immutable";
 import {
   Button,
+  Container,
+  Paper,
   Table,
   TableBody,
   TableCell,
@@ -22,164 +24,172 @@ export default function Home() {
   );
 
   return (
-    <TableContainer>
-      <Table
-        sx={{
-          whiteSpace: "nowrap",
-        }}
-      >
-        <TableHead>
-          <TableRow>
-            <TableCell rowSpan={2}>班級</TableCell>
-            <TableCell rowSpan={2}>代號</TableCell>
-            <TableCell rowSpan={2}>類別</TableCell>
-            <TableCell rowSpan={2}>課程名稱</TableCell>
-            <TableCell rowSpan={2}>學分數</TableCell>
-            <TableCell rowSpan={2}>授課時數</TableCell>
-            <TableCell rowSpan={2}>開課別</TableCell>
-            <TableCell rowSpan={2}>必選修</TableCell>
-            <TableCell colSpan={3} align={"center"}>
-              人數
-            </TableCell>
-            <TableCell rowSpan={2}>校區</TableCell>
-            <TableCell rowSpan={2}>教師</TableCell>
-            <TableCell rowSpan={2}>時間</TableCell>
-            <TableCell rowSpan={2}>備註</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>下限</TableCell>
-            <TableCell>上限</TableCell>
-            <TableCell>目前</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data?.data.map((row) => {
-            const rowSpan = row.teachers.length;
-            return (
-              <>
-                <TableRow
-                  key={row.course_code}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell rowSpan={rowSpan} component="th" scope="row">
-                    {row.class_name}
-                  </TableCell>
-                  <TableCell rowSpan={rowSpan}>{row.course_code}</TableCell>
-                  <TableCell rowSpan={rowSpan}>{row.category}</TableCell>
-                  <TableCell rowSpan={rowSpan}>
-                    <Typography color={"red"}>
-                      {row.name.chinese.tag ? row.name.chinese.tag : ""}
-                    </Typography>
-                    {row.name.chinese.text}
-                    <br />
-                    <Typography color={"red"}>
-                      {row.name.english.tag ? row.name.english.tag : ""}
-                    </Typography>
-                    {row.name.english.text}
-                  </TableCell>
-                  <TableCell rowSpan={rowSpan}>{row.credit}</TableCell>
-                  <TableCell rowSpan={rowSpan}>{row.lecturing_hours}</TableCell>
-                  <TableCell rowSpan={rowSpan}>{row.full_half}</TableCell>
-                  <TableCell rowSpan={rowSpan}>{row.req_select}</TableCell>
-                  <TableCell rowSpan={rowSpan}>{row.enrolled.min}</TableCell>
-                  <TableCell rowSpan={rowSpan}>{row.enrolled.max}</TableCell>
-                  <TableCell rowSpan={rowSpan}>
-                    {row.enrolled.current}
-                  </TableCell>
-                  <TableCell rowSpan={rowSpan}>{row.campus}</TableCell>
-                  <TableCell>{row.teachers[0]}</TableCell>
-                  <TableCell rowSpan={rowSpan}>
-                    {row.times.map((time) => {
-                      return (
-                        <div key={time.day}>
-                          {time.day}
-                          {time.time.length > 0 ? " / " : ""}
-                          {time.time.join(", ")}
-                        </div>
-                      );
-                    })}
-                  </TableCell>
-                  <TableCell rowSpan={rowSpan}>
-                    <form
-                      name="form"
-                      method="post"
-                      target="new"
-                      action="https://shcourse.utaipei.edu.tw/utaipei/ag_pro/ag064_print.jsp"
-                    >
-                      <input
-                        type="hidden"
-                        name="arg01"
-                        value={searchParams.get("year")}
-                      />
-                      <input
-                        type="hidden"
-                        name="arg02"
-                        value={searchParams.get("semester")}
-                      />
-                      <input type="hidden" name="arg04" value={row.syllabus} />
-                      <Button
-                        type="submit"
-                        variant="outlined"
-                        color="secondary"
-                      >
-                        課程大綱
-                      </Button>
-                    </form>
-                    {row.limit && (
+    <Container maxWidth="lg">
+      <TableContainer component={Paper}>
+        <Table
+          sx={{
+            whiteSpace: "nowrap",
+          }}
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell rowSpan={2}>班級</TableCell>
+              <TableCell rowSpan={2}>類別</TableCell>
+              <TableCell rowSpan={2}>課程名稱</TableCell>
+              <TableCell rowSpan={2}>學分數</TableCell>
+              <TableCell rowSpan={2}>授課時數</TableCell>
+              <TableCell rowSpan={2}>開課別</TableCell>
+              <TableCell rowSpan={2}>必選修</TableCell>
+              <TableCell colSpan={3} align={"center"}>
+                人數
+              </TableCell>
+              <TableCell rowSpan={2}>校區</TableCell>
+              <TableCell rowSpan={2}>教師</TableCell>
+              <TableCell rowSpan={2}>時間</TableCell>
+              <TableCell rowSpan={2}>備註</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>下限</TableCell>
+              <TableCell>上限</TableCell>
+              <TableCell>目前</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data?.data.map((row) => {
+              const rowSpan = row.teachers.length;
+              return (
+                <>
+                  <TableRow
+                    key={row.course_code}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell rowSpan={rowSpan} component="th" scope="row">
+                      {row.class_name}
+                      <br />
+                      {row.course_code}
+                    </TableCell>
+                    <TableCell rowSpan={rowSpan}>{row.category}</TableCell>
+                    <TableCell rowSpan={rowSpan}>
+                      <Typography color={"red"}>
+                        {row.name.chinese.tag ? row.name.chinese.tag : ""}
+                      </Typography>
+                      {row.name.chinese.text}
+                      <br />
+                      <Typography color={"red"}>
+                        {row.name.english.tag ? row.name.english.tag : ""}
+                      </Typography>
+                      {row.name.english.text}
+                    </TableCell>
+                    <TableCell rowSpan={rowSpan}>{row.credit}</TableCell>
+                    <TableCell rowSpan={rowSpan}>
+                      {row.lecturing_hours}
+                    </TableCell>
+                    <TableCell rowSpan={rowSpan}>{row.full_half}</TableCell>
+                    <TableCell rowSpan={rowSpan}>{row.req_select}</TableCell>
+                    <TableCell rowSpan={rowSpan}>{row.enrolled.min}</TableCell>
+                    <TableCell rowSpan={rowSpan}>{row.enrolled.max}</TableCell>
+                    <TableCell rowSpan={rowSpan}>
+                      {row.enrolled.current}
+                    </TableCell>
+                    <TableCell rowSpan={rowSpan}>{row.campus}</TableCell>
+                    <TableCell>{row.teachers[0]}</TableCell>
+                    <TableCell rowSpan={rowSpan}>
+                      {row.times.map((time) => {
+                        return (
+                          <div key={time.day}>
+                            {time.day}
+                            {time.time.length > 0 ? " / " : ""}
+                            {time.time.join(", ")}
+                          </div>
+                        );
+                      })}
+                    </TableCell>
+                    <TableCell rowSpan={rowSpan}>
                       <form
-                        name="notes"
+                        name="form"
                         method="post"
                         target="new"
-                        action="https://shcourse.utaipei.edu.tw/utaipei/ag_pro/ag203_limit.jsp"
+                        action="https://shcourse.utaipei.edu.tw/utaipei/ag_pro/ag064_print.jsp"
                       >
                         <input
                           type="hidden"
-                          name="ls_year"
+                          name="arg01"
                           value={searchParams.get("year")}
                         />
                         <input
                           type="hidden"
-                          name="ls_sms"
+                          name="arg02"
                           value={searchParams.get("semester")}
                         />
                         <input
                           type="hidden"
-                          name="data"
-                          value={row.course_code}
+                          name="arg04"
+                          value={row.syllabus}
                         />
                         <Button
                           type="submit"
                           variant="outlined"
                           color="secondary"
                         >
-                          課程限修條件
+                          課程大綱
                         </Button>
                       </form>
-                    )}
-                    Mixed: {row.mixed} <br />
-                    Note: {row.notes} <br />
-                  </TableCell>
-                </TableRow>
-                {row.teachers.map((teacher, index) => {
-                  if (index === 0) {
-                    return null;
-                  }
-                  return (
-                    <TableRow
-                      key={teacher}
-                      sx={{
-                        "&:last-child td, &:last-child th": { border: 0 },
-                      }}
-                    >
-                      <TableCell>{teacher}</TableCell>
-                    </TableRow>
-                  );
-                })}
-              </>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
+                      {row.limit && (
+                        <form
+                          name="notes"
+                          method="post"
+                          target="new"
+                          action="https://shcourse.utaipei.edu.tw/utaipei/ag_pro/ag203_limit.jsp"
+                        >
+                          <input
+                            type="hidden"
+                            name="ls_year"
+                            value={searchParams.get("year")}
+                          />
+                          <input
+                            type="hidden"
+                            name="ls_sms"
+                            value={searchParams.get("semester")}
+                          />
+                          <input
+                            type="hidden"
+                            name="data"
+                            value={row.course_code}
+                          />
+                          <Button
+                            type="submit"
+                            variant="outlined"
+                            color="secondary"
+                          >
+                            課程限修條件
+                          </Button>
+                        </form>
+                      )}
+                      {row.notes} <br />
+                      {row.mixed && "合班班級：" + row.mixed}
+                    </TableCell>
+                  </TableRow>
+                  {row.teachers.map((teacher, index) => {
+                    if (index === 0) {
+                      return null;
+                    }
+                    return (
+                      <TableRow
+                        key={teacher}
+                        sx={{
+                          "&:last-child td, &:last-child th": { border: 0 },
+                        }}
+                      >
+                        <TableCell>{teacher}</TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 }
